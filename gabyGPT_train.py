9326a77bc53460a7049b4440e795eeb678abb974
GabriellaAH <gabi.a.herke@gmail.com>
2024-01-02 08:45:24 +0000

    fix get_bach issue, add comments

--- gabyGPT_train.py
+++ gabyGPT_train.py
@@ -12,16 +12,16 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'  # Use GPU if available
 # Hyperparameters for the model
 batch_size = 16
 block_size = 128
-max_iters = 5000
+max_iters = 10000
 learning_rate = 4e-4
 eval_iters = 500
 n_embd = 1140
 n_head = 32
 n_layer = 36
 dropout = 0.2
-load_model = False
+load_model = True
 load_model_name = 'gabyGPT-00.pkl'
-save_model_name = 'gabyGPT-00.pkl'
+save_model_name = 'gabyGPT-01.pkl'
 
 print(device)
 
@@ -32,8 +32,16 @@ vocab_size = 45000
 encode = lambda text: tokenizer.encode(text).ids
 decode = lambda token_ids: tokenizer.decode(token_ids)
 
-# Function to get a random chunk of text from the dataset
 def get_random_chunk(split):
+    """
+    Retrieves a random chunk of text from the dataset.
+
+    Args:
+        split (str): Indicates the dataset split to use ('train' or 'val').
+
+    Returns:
+        torch.Tensor: A tensor containing encoded token IDs from the random text chunk.
+    """
     filename = "./output_train.txt" if split == 'train' else "./output_val.txt"
     with open(filename, 'rb') as f:
         with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
@@ -51,18 +59,34 @@ def get_random_chunk(split):
 
     return data
 
-# Function to create a batch for training/validation
 def get_batch(split):
+    """
+    Creates a batch of data for training or validation.
+
+    Args:
+        split (str): Indicates the dataset split to use ('train' or 'val').
+
+    Returns:
+        tuple: A tuple containing two tensors (input and target) for the batch.
+    """    
     data = get_random_chunk(split)
+    if len(data) <= block_size:
+        data = torch.cat([data, torch.zeros(block_size - len(data))], dim=0)
+    
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i + block_size] for i in ix])
     y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
 
-# Function to estimate the loss for training and validation sets
 @torch.no_grad()
 def estimate_loss():
+    """
+    Estimates the loss for both training and validation sets.
+
+    Returns:
+        dict: A dictionary with mean loss values for 'train' and 'val' splits.
+    """    
     out = {}
     model.eval()
     for split in ['train', 'val']:
@@ -87,6 +111,15 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
+        """
+        Forward pass for a single attention head.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after applying the attention mechanism.
+        """        
         B, T, C = x.shape
         k = self.key(x)  # Key
         q = self.query(x)  # Query
@@ -107,6 +140,15 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
+        """
+        Forward pass for multi-head attention.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after applying multi-head attention.
+        """        
         out = torch.cat([h(x) for h in self.heads], dim=-1)  # Concatenate the output of all heads
         out = self.dropout(self.proj(out))  # Project back to the embedding dimension
         return out
@@ -123,6 +165,15 @@ class FeedFoward(nn.Module):
         )
 
     def forward(self, x):
+        """
+        Forward pass for the feedforward network.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after passing through the feedforward network.
+        """        
         return self.net(x)
 
 # The Block class represents one block of the Transformer model
@@ -136,6 +187,15 @@ class Block(nn.Module):
         self.ffwd = FeedFoward(n_embd)
 
     def forward(self, x):
+        """
+        Forward pass for a single Transformer block.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after processing through the Transformer block.
+        """        
         y = self.ln1(x)
         y = self.sa(y)
         x = x + y  # Apply residual connection
@@ -157,7 +217,20 @@ class GPTLanguageModel(nn.Module):
         self.apply(self._init_weights)
 
     def _init_weights(self, module):
-        # Initialize weights for linear and embedding layers
+        """
+        Initializes weights for the linear and embedding layers of the model.
+
+        This method applies a specific weight initialization strategy to different types of layers.
+        For linear layers, it initializes the weights from a normal distribution with a mean of 0.0 and a standard deviation of 0.02.
+        If the linear layer has a bias term, it initializes the bias to zero.
+        For embedding layers, it initializes the weights from a normal distribution with a mean of 0.0 and a standard deviation of 0.02.
+
+        Args:
+            module (nn.Module): A PyTorch module from the model, typically a layer.
+
+        Note:
+            This method is designed to be used with the `apply` method of `nn.Module`, which applies a function recursively to every submodule.
+        """
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
             if module.bias is not None:
@@ -166,6 +239,16 @@ class GPTLanguageModel(nn.Module):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, index, targets=None):
+        """
+        Forward pass for the GPT language model.
+
+        Args:
+            index (torch.Tensor): Tensor of token indices.
+            targets (torch.Tensor, optional): Target token indices for loss calculation.
+
+        Returns:
+            tuple: A tuple containing logits and loss (if targets are provided).
+        """        
         B, T = index.shape
         tok_emb = self.token_embedding_table(index)  # Token embeddings
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # Positional embeddings
@@ -188,7 +271,16 @@ class GPTLanguageModel(nn.Module):
         return logits, loss
 
     def generate(self, index, max_new_tokens):
-        # Generate text given an input index
+        """
+        Generates text given an input index.
+
+        Args:
+            index (torch.Tensor): Tensor of starting token indices.
+            max_new_tokens (int): Maximum number of new tokens to generate.
+
+        Returns:
+            torch.Tensor: Tensor of token indices including the generated tokens.
+        """
         for _ in range(max_new_tokens):
             logits, loss = self.forward(index)
             logits = logits[:, -1, :]  # Take the last step
--- readme.md
+++ readme.md
@@ -5,16 +5,19 @@ This repository hosts GabyGPT, a learning project focused on understanding and i
 The Transformer model implemented here is akin to the GPT (Generative Pre-trained Transformer) models, serving as an educational tool to explore the intricacies of Transformers in natural language tasks.
 
 ## Repository Structure
-gabyGPT_train.py: This file contains the full architecture of the Transformer model along with the training loop. It is the core of the project, where the model is defined, trained, and saved.
-test_bot.py: A simple script to test and interact with the trained model. It serves as a basic demonstration of how the model can be used for generating text.
+- `gabyGPT_train.py`: This file contains the full architecture of the Transformer model along with the training loop. It is the core of the project, where the model is defined, trained, and saved.
+- `test_bot.py`: A simple script to test and interact with the trained model. It serves as a basic demonstration of how the model can be used for generating text.
+- `BPETokenizer.py`: Implements a tokenizer using the Byte Pair Encoding (BPE) algorithm. This script is crucial for preprocessing text data into a format suitable for the Transformer model.
+
+
 ## Implementation Details
 The model, built using PyTorch, features several key components of the Transformer architecture:
 
-Multi-Head Self-Attention: Allows the model to dynamically focus on different parts of the input sequence.
-Positional Encoding: Provides the model with information about the order of tokens in the sequence.
-Layer Normalization and Residual Connections: Employed in each Transformer block to help in stabilizing and speeding up the training process.
-Feed-Forward Network: A component of each Transformer block for processing the sequence data.
-Training Loop: Facilitates the training of the model on text data, enabling it to predict subsequent tokens in a sequence.
+- **Multi-Head Self-Attention**: Allows the model to dynamically focus on different parts of the input sequence.
+- **Positional Encoding**: Provides the model with information about the order of tokens in the sequence.
+- **Layer Normalization and Residual Connections**: Employed in each Transformer block to help in stabilizing and speeding up the training process.
+- **Feed-Forward Network**: A component of each Transformer block for processing the sequence data.
+- **Training Loop**: Facilitates the training of the model on text data, enabling it to predict subsequent tokens in a sequence.
 Usage
 To use this project, you need Python 3 and PyTorch. Train the model using gabyGPT_train.py. After training, you can interact with the model using test_bot.py, which provides a simple interface to generate text based on the trained model.
 
